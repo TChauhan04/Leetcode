303cc5be68b3cc64c419f1381ce062e8c045ef95
Tanishtha Chauhan <112814396+TChauhan04@users.noreply.github.com>
2024-06-30 23:29:21 +0530

    Update duplicate_zeroes.cpp

--- duplicate_zeroes.cpp
+++ duplicate_zeroes.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <vector>
 
-void duplicateZeros(std::vector<int>& arr) {
+void duplicateZeros(std::vector<int>& arr) 
+{
     int length = arr.size();
     int i = 0;
     
-    while (i < length) {
-        if (arr[i] == 0) {
+    while (i < length) 
+    {
+        if (arr[i] == 0) 
+        {
             // Shift elements to the right
-            for (int j = length - 1; j > i; --j) {
+            for (int j = length - 1; j > i; --j) 
+            {
                 arr[j] = arr[j - 1];
             }
             // Duplicate the zero
             ++i;  // Move past the duplicated zero
-            if (i < length) {  // Avoid index out of range
+            if (i < length) 
+            {  // Avoid index out of range
                 arr[i] = 0;
             }
         }
@@ -21,13 +26,15 @@ void duplicateZeros(std::vector<int>& arr) {
     }
 }
 
-int main() {
+int main() 
+{
     std::vector<int> arr = {1, 0, 2, 3, 0, 4, 5, 0};
     
     duplicateZeros(arr);
     
     // Print the modified array
-    for (int num : arr) {
+    for (int num : arr) 
+    {
         std::cout << num << " ";
     }
     std::cout << std::endl;
