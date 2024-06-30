8ee20243c4aa0937a86556b05ea5e1c6c46073bf
Tanishtha Chauhan <112814396+TChauhan04@users.noreply.github.com>
2024-06-30 23:28:25 +0530

    Update merged_sort_array.cpp

--- merged_sort_array.cpp
+++ merged_sort_array.cpp
@@ -1,17 +1,21 @@
 #include <vector>
 #include <iostream>
 
-void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) 
+{
     int p1 = m - 1; // Pointer for nums1
     int p2 = n - 1; // Pointer for nums2
     int p = m + n - 1; // Pointer for the end of nums1
     
     // While there are elements to compare
-    while (p1 >= 0 && p2 >= 0) {
-        if (nums1[p1] > nums2[p2]) {
+    while (p1 >= 0 && p2 >= 0) 
+    {
+        if (nums1[p1] > nums2[p2])
+        {
             nums1[p] = nums1[p1];
             p1--;
-        } else {
+        } else 
+        {
             nums1[p] = nums2[p2];
             p2--;
         }
@@ -19,14 +23,16 @@ void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
     }
     
     // If there are remaining elements in nums2, copy them
-    while (p2 >= 0) {
+    while (p2 >= 0) 
+    {
         nums1[p] = nums2[p2];
         p2--;
         p--;
     }
 }
 
-int main() {
+int main() 
+{
     std::vector<int> nums1 = {1, 2, 3, 0, 0, 0};
     int m = 3;
     std::vector<int> nums2 = {2, 5, 6};
@@ -35,7 +41,8 @@ int main() {
     merge(nums1, m, nums2, n);
 
     std::cout << "Merged array: ";
-    for (int num : nums1) {
+    for (int num : nums1) 
+    {
         std::cout << num << " ";
     }
     std::cout << std::endl;
