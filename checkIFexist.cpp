22f3f4fa6532dfe7cf7bb955706de4a183eba100
Tanishtha Chauhan <112814396+TChauhan04@users.noreply.github.com>
2024-07-15 23:29:08 +0530

    Add files via upload

---
+++ checkIFexist.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        std::unordered_map<int, int> value_indices;
+
+    for (int index = 0; index < arr.size(); ++index) {
+        int value = arr[index];
+        // Check if value * 2 or value / 2 exists in the map
+        if (value_indices.find(value * 2) != value_indices.end() ||
+            (value % 2 == 0 && value_indices.find(value / 2) != value_indices.end())) {
+            return true;
+        }
+        value_indices[value] = index;
+    }
+    
+    return false;
+}
+
+int main() {
+    std::vector<int> arr = {10, 2, 5, 3};
+    if (checkIfExist(arr)) {
+        std::cout << "True" << std::endl;
+    } else {
+        std::cout << "False" << std::endl;
+    }
+
+    return 0;
+    }
+};
